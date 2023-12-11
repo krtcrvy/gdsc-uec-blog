4659a9bbaabb3ee2a2f6efef72440447c8d860ae
Kurt Carvey Cadenas <kurtcarveycadenas2001@gmail.com>
2023-12-12 00:39:10 +0800

    Update show.blade.php

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,9 +1,9 @@
 <x-app-layout>
-    <x-header/>
+    <x-header />
     <main class="bg-white pb-16 pt-8 antialiased dark:bg-gray-900 lg:pb-24 lg:pt-16">
-        @if(session()->has('success'))
-            <div class="max-w-2xl mx-auto mb-6">
-                <x-success-alert/>
+        @if (session()->has('success'))
+            <div class="mx-auto mb-6 max-w-2xl">
+                <x-success-alert />
             </div>
         @endif
         <div class="mx-auto flex max-w-screen-xl justify-between px-4">
@@ -12,14 +12,15 @@
                 <header class="not-format mb-4 lg:mb-6">
                     <address class="mb-6 flex items-center not-italic">
                         <div class="mr-3 inline-flex items-center text-sm text-gray-900 dark:text-white">
-                            <img alt="{{ Auth::user()->profile_name }}" class="mr-4 h-16 w-16 rounded-full"
-                                 src="{{ Auth::user()->profile_photo_url }}">
+                            <img alt="{{ $post->user->name }}" class="mr-4 h-16 w-16 rounded-full"
+                                src="{{ $post->user->profile_photo_url }}">
                             <div>
-                                <a class="text-xl font-bold text-gray-900 dark:text-white" href="#" rel="author">
-                                    {{ Auth::user()->name }}
+                                <a class="text-xl font-bold text-gray-900 dark:text-white" href="#"
+                                    rel="author">
+                                    {{ $post->user->name }}
                                 </a>
                                 <p class="text-base text-gray-500 dark:text-gray-400">
-                                    {{ Auth::user()->email }}
+                                    {{ $post->user->email }}
                                 </p>
                                 <p class="text-base text-gray-500 dark:text-gray-400">
                                     {{ $post->created_at->diffForHumans() }}
@@ -37,13 +38,12 @@
 
                 </header>
 
-                <div
-                    class="prose dark:prose-invert sm:prose-sm md:prose-base lg:prose-lg prose-img:rounded-lg">
+                <div class="prose dark:prose-invert sm:prose-sm md:prose-base lg:prose-lg prose-img:rounded-lg">
                     {!! $post->body !!}
                 </div>
             </article>
         </div>
     </main>
 
-    <x-footer/>
+    <x-footer />
 </x-app-layout>
